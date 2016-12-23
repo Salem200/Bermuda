2b8aab5178497912c637fae9837ff7b49e46611f
Salem200 <longcatofdoom@gmail.com>
2016-12-23 18:01:49 +0100

    staffan springer

--- main.py
+++ main.py
@@ -13,28 +13,59 @@ staffan = pygame.image.load("staffan.jpg")
 staffanx = 1
 staffany = 1
 
-#set up the window:
+rKeyPressed = False
+lKeyPressed = False
+uKeyPressed = False
+dKeyPressed = False
+
+#set up the window
 DISPLAYSURF = pygame.display.set_mode((wWidth, wHeight), 0, 32)
 pygame.display.set_caption("Bermuda")
 
-#main game loop:
+#main game loop
 while True:
     DISPLAYSURF.fill(WHITE)
+
+    if rKeyPressed:
+        staffanx +=5
+    if lKeyPressed:
+        staffanx -=5
+    if uKeyPressed:
+        staffany -=5
+    if dKeyPressed:
+        staffany +=5
     
-    #event handling loop:
+    #event handling loop
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
             pygame.quit()
             sys.exit()
 
         if event.type == KEYDOWN and event.key == K_RIGHT:
-            staffanx += 5
+            if not rKeyPressed:
+                rKeyPressed = True
+                staffanx += 5
         if event.type == KEYDOWN and event.key == K_LEFT:
-            staffanx -= 5
+            if not lKeyPressed:
+                lKeyPressed = True
+                staffanx -= 5
         if event.type == KEYDOWN and event.key == K_UP:
-            staffany -= 5
+            if not uKeyPressed:
+                uKeyPressed = True
+                staffany -= 5
         if event.type == KEYDOWN and event.key == K_DOWN:
-            staffany += 5
+            if not dKeyPressed:
+                dKeyPressed = True
+                staffany += 5
+
+        if event.type == KEYUP and event.key == K_RIGHT:
+            rKeyPressed = False
+        if event.type == KEYUP and event.key == K_LEFT:
+            lKeyPressed = False
+        if event.type == KEYUP and event.key == K_UP:
+            uKeyPressed = False
+        if event.type == KEYUP and event.key == K_DOWN:
+            dKeyPressed = False
 
     DISPLAYSURF.blit(staffan, (staffanx, staffany))
 
